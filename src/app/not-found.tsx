733c574d20ec28efdecaa8bd12b35fe15202ea97
FarhanTips <farhanakbor02@gmail.com>
2026-09-23 20:59:39 +0600

    loading and not-found added properly

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GlobalNotFound = () => {
+    return (
+        <div className='min-h-screen flex justify-center flex-col items-center'>
+            <h1 className='text-3xl font-bold'>404 - Page Not Found</h1>
+            <p className='text-2xl '>The page you are looking for does not exist.</p>
+        </div>
+    );
+};
+
+export default GlobalNotFound;
