45e221fcfe971f1d7cbb0c9b82993d6119a16896
FarhanTips <farhanakbor02@gmail.com>
2026-09-23 22:32:49 +0600

    Responsiveness applied for Library

--- src/Components/homeLibrary.tsx
+++ src/Components/homeLibrary.tsx
@@ -6,7 +6,7 @@ const HomeLibrary = async () => {
     const fitLogs = await getFitLogs();
     return (
         <div className="w-10/12 mx-auto">
-            <div className="grid grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 {fitLogs.map(fitLog => <FitLogCard key={fitLog.id} fitLog={fitLog}></FitLogCard>)}
             </div>
 
